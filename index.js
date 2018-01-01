95983d9143477b04ae7d026936070c6035a979b6
Patrick McCombs <pa.mccombs1@gmail.com>
2018-01-01 14:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,5 @@ function sayHiToGrandma(string)  {
   if (string.toLowerCase() === lowercase)  {
     return 'I can\'t hear you!'
   }
- else if (string.toUpperCase() === uppercase) {
-    return 'YES INDEED!'
-  }
 }
+return string.toUpperCase
